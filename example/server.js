e18f4817e40bc720e7ac0873766506b599656228
Axel Mousset <pymzorr@gmail.com>
2016-12-13 16:45:42 +0900

    Fix resolution and orbit controls

--- example/server.js
+++ example/server.js
@@ -3,4 +3,4 @@ var express = require('express');
 var app = express();
 app.use(express.static('../'));
 app.get('/myStream.jpg', new MjpegProxy('http://192.168.43.134:8080/?action=stream').proxyRequest);
-app.listen(8080);
+app.listen(8000);
